08f9c9c11105c49ff2712da0c9b8dde563ee0516
jenstem <jennifer.stemkowski@gmail.com>
2024-05-02 17:50:42 -0400

    Run multiple tests with PyTest

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,5 +1,11 @@
-from app.demo import add
+from app.demo import add, subtract, multiply
 
 
 def test_add():
     assert add(10, 20) == 30
+
+def test_subtract():
+    assert subtract(20, 10) == 10
+
+def test_multiply():
+    assert multiply(10, 2) == 20
