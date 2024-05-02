7f283dfe1898cfc910d105a5a1a8c51b6f1a39fb
jenstem <jennifer.stemkowski@gmail.com>
2024-05-02 15:34:06 -0400

    Create multiple asserts

--- test_demo.py
+++ test_demo.py
@@ -4,7 +4,9 @@ import demo
 
 class TestDemo(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(demo.add(2, 3), 5)
+        self.assertEqual(demo.add(2, 2), 4)
+        self.assertEqual(demo.add(10, 4), 14)
+        self.assertEqual(demo.add(23, 7), 30)
 
 
 if __name__ == '__main__':
