61191aa9c1997fa00a0dac6dc4b05ed25c7d458f
jenstem <jennifer.stemkowski@gmail.com>
2024-05-02 16:07:21 -0400

    Test multiple methods in a class

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 class Calculate:
     def add(self, a, b):
         return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
--- test_demo.py
+++ test_demo.py
@@ -3,10 +3,22 @@ import demo
 
 
 class TestDemo(unittest.TestCase):
-    def test_demo(self):
+
+    def test_add(self):
         calculate = demo.Calculate()
         self.assertEqual(calculate.add(1, 2), 3)
 
+    def test_subtract(self):
+        calculate = demo.Calculate()
+        self.assertEqual(calculate.subtract(3, 2), 1)
+
+    def test_multiply(self):
+        calculate = demo.Calculate()
+        self.assertEqual(calculate.multiply(3, 2), 6)
+
+    def test_divide(self):
+        calculate = demo.Calculate()
+        self.assertEqual(calculate.divide(10, 2), 5)
 
 
 if __name__ == '__main__':
