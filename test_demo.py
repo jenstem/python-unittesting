4b2c0d9bfbc4ed18bfa41398e61e69cf4bfc5f62
jenstem <jennifer.stemkowski@gmail.com>
2024-05-02 15:29:51 -0400

    Set name = main for easier cmd

--- test_demo.py
+++ test_demo.py
@@ -2,7 +2,10 @@ import unittest
 import demo
 
 
-
 class TestDemo(unittest.TestCase):
     def test_add(self):
         self.assertEqual(demo.add(2, 3), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
