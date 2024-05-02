8b9be09d4a3897246fd7dca921c16184340ca471
jenstem <jennifer.stemkowski@gmail.com>
2024-05-02 17:03:35 -0400

    Add set up method

--- test_demo.py
+++ test_demo.py
@@ -3,22 +3,20 @@ import demo
 
 
 class TestDemo(unittest.TestCase):
+    def setUp(self):
+        self.calculate = demo.Calculate()
 
     def test_add(self):
-        calculate = demo.Calculate()
-        self.assertEqual(calculate.add(1, 2), 3)
+        self.assertEqual(self.calculate.add(1, 2), 3)
 
     def test_subtract(self):
-        calculate = demo.Calculate()
-        self.assertEqual(calculate.subtract(3, 2), 1)
+        self.assertEqual(self.calculate.subtract(3, 2), 1)
 
     def test_multiply(self):
-        calculate = demo.Calculate()
-        self.assertEqual(calculate.multiply(3, 2), 6)
+        self.assertEqual(self.calculate.multiply(3, 2), 6)
 
     def test_divide(self):
-        calculate = demo.Calculate()
-        self.assertEqual(calculate.divide(10, 2), 5)
+        self.assertEqual(self.calculate.divide(10, 2), 5)
 
 
 if __name__ == '__main__':
