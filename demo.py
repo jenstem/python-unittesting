e97e571a1b07f338b3ed9b9bd468675815fe0db9
jenstem <jennifer.stemkowski@gmail.com>
2024-05-02 16:02:58 -0400

    Test a class method

--- demo.py
+++ demo.py
@@ -1,14 +1,3 @@
-def add(a, b):
-    return a + b
-
-
-def sub(a, b):
-    return a - b
-
-
-def multiple(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
+class Calculate:
+    def add(self, a, b):
+        return a + b
--- test_demo.py
+++ test_demo.py
@@ -3,26 +3,10 @@ import demo
 
 
 class TestDemo(unittest.TestCase):
+    def test_demo(self):
+        calculate = demo.Calculate()
+        self.assertEqual(calculate.add(1, 2), 3)
 
-    def test_add(self):
-        self.assertEqual(demo.add(2, 2), 4)
-        self.assertEqual(demo.add(10, 4), 14)
-        self.assertEqual(demo.add(23, 7), 30)
-
-    def test_sub(self):
-        self.assertEqual(demo.sub(4, 2), 2)
-        self.assertEqual(demo.sub(10, 4), 6)
-        self.assertEqual(demo.sub(23, 7), 16)
-
-    def test_multiple(self):
-        self.assertEqual(demo.multiple(2, 2), 4)
-        self.assertEqual(demo.multiple(10, 4), 40)
-        self.assertEqual(demo.multiple(20, 5), 100)
-
-    def test_divide(self):
-        self.assertEqual(demo.divide(4, 2), 2)
-        self.assertEqual(demo.divide(10, 2), 5)
-        self.assertEqual(demo.divide(21, 7), 3)
 
 
 if __name__ == '__main__':
