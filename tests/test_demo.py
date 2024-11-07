30e0c5780c03fc0e6185fafee906ccead86fbe7d
Jen <124745888+jenstem@users.noreply.github.com>
2024-11-07 15:45:38 -0500

    Add docstring

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -2,6 +2,9 @@ import pytest
 from app.demo import ShoppingCart
 
 def test_add_item():
+    """
+    Test the functionality of adding an item to the shopping cart.
+    """
     cart = ShoppingCart()
     cart.add_item("apple", 2)
     assert cart.get_item_count("apple") == 2
